1c8b73d29a73da7585877c6e042861b698aebb18
Nikita Sapunov <kiton1994@gmail.com>
2017-04-17 12:24:55 +0300

    Mathjax support on a taskpage

--- app/judge/ui/static/app.js
+++ app/judge/ui/static/app.js
@@ -262,4 +262,16 @@ function UserPageCtrl($scope, $http) {
             }, true);
         }
     };
+})
+
+.directive("mathjaxBind", function() {
+    return {
+        restrict: "A",
+        controller: ["$scope", "$element", "$attrs", function($scope, $element, $attrs) {
+            $scope.$watch($attrs.mathjaxBind, function(value) {
+                $element.text(value == undefined ? "" : value);
+                MathJax.Hub.Queue(["Typeset", MathJax.Hub, $element[0]]);
+            });
+        }]
+    };
 });
--- app/judge/ui/static/js/directives.js
+++ app/judge/ui/static/js/directives.js
@@ -10,4 +10,16 @@ angular.module('judge')
             }, true);
         }
     };
+})
+
+.directive("mathjaxBind", function() {
+    return {
+        restrict: "A",
+        controller: ["$scope", "$element", "$attrs", function($scope, $element, $attrs) {
+            $scope.$watch($attrs.mathjaxBind, function(value) {
+                $element.text(value == undefined ? "" : value);
+                MathJax.Hub.Queue(["Typeset", MathJax.Hub, $element[0]]);
+            });
+        }]
+    };
 });
