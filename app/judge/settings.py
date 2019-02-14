8124208eddaed7086fbcb746e7f2763c0f2302b5
Nikita Sapunov <kiton1994@gmail.com>
2019-02-14 12:04:06 +0300

    Загружается список нотификаций Добавлен фавикон

--- app/judge/settings.py
+++ app/judge/settings.py
@@ -176,6 +176,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
 }
 
 STATIC_ROOT = os.path.join(DATA_DIR, 'static')
